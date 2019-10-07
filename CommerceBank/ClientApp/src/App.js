8d2039328b55b6a541dab939335112e7624dd50c
KHarris75 <36537065+KHarris75@users.noreply.github.com>
2019-10-06 23:55:08 -0500

    Finished Blank UI Skeletons and changed some text for prototype

--- CommerceBank/ClientApp/src/App.js
+++ CommerceBank/ClientApp/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
+import { Notifications } from './components/Notifications';
+import { CustomAlerts } from './components/CustomAlerts';
+import { UserSettings } from './components/UserSettings';
 import './custom.css'
 
 export default class App extends Component {
@@ -10,7 +13,10 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/' component={Notifications} />
+            <Route exact path='/' component={CustomAlerts} />
+            <Route exact path='/' component={UserSettings} />
       </Layout>
     );
   }
--- CommerceBank/ClientApp/src/components/NavMenu.js
+++ CommerceBank/ClientApp/src/components/NavMenu.js
@@ -26,13 +26,22 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">CommerceBank</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">Commerce Bank</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/Notifications">Notifications</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/CustomAlerts">Custom Alerts</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/UserSettings">Settings</NavLink>
+                            </NavItem>
               </ul>
             </Collapse>
           </Container>
