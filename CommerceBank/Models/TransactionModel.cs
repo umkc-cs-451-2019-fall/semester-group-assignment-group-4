514026431d5703ac4b4c333b90c5f3876fa50467
KHarris75 <36537065+KHarris75@users.noreply.github.com>
2019-10-06 21:05:22 -0500

    Finished designing prototype transaction table

--- CommerceBank/Models/TransactionModel.cs
+++ CommerceBank/Models/TransactionModel.cs
@@ -8,6 +8,7 @@ namespace CommerceBank.Models
     public class TransactionModel
     {
         public int id { get; set; }
+        public int account { get; set; }
         public double amount { get; set; }
         public string date { get; set; }
         public string details { get; set; }
