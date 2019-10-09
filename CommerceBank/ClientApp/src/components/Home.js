c149b50ec4769adeb18aa3c5e2380133881a2ecd
KHarris75 <36537065+KHarris75@users.noreply.github.com>
2019-10-09 11:31:11 -0500

    Added text descriptions to Reports and Custom Alerts Collapsibles

--- CommerceBank/ClientApp/src/components/Home.js
+++ CommerceBank/ClientApp/src/components/Home.js
@@ -11,6 +11,31 @@ export class Home extends Component {
         this.GetInitialLoadTransactionData();
         this.GetSavingsLoadTransactionData();
     }
+
+    static renderReportsDescription() {
+        return (
+
+            <div>
+                <p> </p>
+            <p>This is where we plan to implement the interface for customers to view reports based on their business rules and notifications.</p>
+            <p></p>
+            <p>Customers will easily be able set the parameters for the type of data they do or do not want to see on the screen.</p>
+            <p>They also should be able to easily export this data to excel, along with the transaction data related to their accounts.</p>
+        </div>
+        );
+    }
+    static renderCustomAlertsDescription() {
+        return (
+
+            <div>
+                <p> </p>
+            <p>Here we will have the interface that gives customers CRUD operations for the Business Rules.</p>
+            <p></p>
+            <p>Business Rules are events that customers want to receive notifications for and that the application will generate reports for.</p>
+            <p>We decided that "Custom Alerts" made a bit more sense as a descriptor for "Business Rules" on the Front-End. They're interchangeable.</p>
+        </div>
+        );
+    }
     static renderTransactionTable(TransactionData) {
         return (
             <table className='table table-striped' aria-labelledby="tableLable">
@@ -45,10 +70,14 @@ export class Home extends Component {
         // else it equals the transaction table function data found above)
         let contents = this.state.loading ? <p><em>Loading...</em></p> : Home.renderTransactionTable(this.state.TransactionData);
         let contents1 = this.state.loading ? <p><em>Loading...</em></p> : Home.renderTransactionTable(this.state.SavingsData);
+
+        let reportsDescrip = this.state.loading ? <p><em>Loading...</em></p> : Home.renderReportsDescription();
+        let alertsDescrip = this.state.loading ? <p><em>Loading...</em></p> : Home.renderCustomAlertsDescription();
+
         return (
             <div>
                 <h1 id="tabelLabel" >Hi, Class!</h1>
-                <p>This is the sample Home page for a customer, below is a sample table of transaction data</p>
+                <p>This is the sample Home page for a customer, below is a prototype of the main User Interface</p>
                 <div class="homeContainterDiv">
                     <CollapsibleComponent header="Checking" content={contents} componentID="1" />
                 </div>
@@ -56,10 +85,10 @@ export class Home extends Component {
                     <CollapsibleComponent header="Savings" content={contents1} componentID="4" />
                 </div>
                 <div class="homeContainterDiv">
-                    <CollapsibleComponent header="Reports" content={contents1} componentID="2"/>
+                    <CollapsibleComponent header="Reports" content={reportsDescrip} componentID="2"/>
                 </div>
                 <div class="homeContainterDiv">
-                    <CollapsibleComponent header="Custom Alerts" content={contents} componentID="3"/>
+                    <CollapsibleComponent header="Custom Alerts" content={alertsDescrip} componentID="3"/>
                 </div>
             </div>
         );
